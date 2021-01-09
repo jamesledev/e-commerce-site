81f302a935f1b1ca9181cf811350b712123490a3
jamesledev <74250657+jamesledev@users.noreply.github.com>
2021-01-09 01:41:16 +0000

    Navbar and Products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Grid} from '@material-ui/core'
+
+import Product from './Product/Product'
+import useStyles from './styles'
+
+
+
+
+const Products =  ({products}) => {
+  const classes = useStyles();
+
+  return (
+  <main className={classes.content}>
+    <div className={classes.toolbar} />
+    <Grid container justify='center' spacing={4} alignItems="center">
+      {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            {/* xs sm md lg  talking about device size*/}
+            <Product product={product}/>
+          
+            </Grid>
+      ))}
+    </Grid>
+  </main>
+
+  )
+};
+
+export default Products
