dd7d25cd11464b112c89fbddaff22c4be495d0a7
jamesledev <74250657+jamesledev@users.noreply.github.com>
2021-01-11 23:31:14 +0000

    done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = ({ totalItems }) => {
         <Toolbar>
           <Typography component={Link} to='/' variant ='h6' className={classes.title} color='inherit'>
             <img src={logo} alt="Commerce.js" height ='25px' className={classes.image} />
-            James' Shop
+            James' DEMO Shop
           </Typography>
           <div className={classes.grow} />
           {location.pathname === '/' && (
