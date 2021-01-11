5abdb7d010c97f341c47ff36bdd067121736b1ad
jamesledev <74250657+jamesledev@users.noreply.github.com>
2021-01-11 15:49:45 +0000

    Fnished cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,14 +5,18 @@ import { Link } from 'react-router-dom'
 import useStyles from './styles'
 import CartItem from './CartItem/CartItem'
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, 
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart
+}) => {
  
   const classes = useStyles()
 
   const EmptyCart = () => (
     <Typography variant="subtitle1">
-      You have no items in your shopping cart
-      <Link to='/' classesName={classes.link}>Start adding some!</Link>
+      You have no items in your shopping cart 
+      <Link to='/' className={classes.link}> Start adding some!</Link>
     </Typography>
   );
 
@@ -21,16 +25,16 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <CartItem item={item} />
+            <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}/>
           </Grid>
         ))}
       </Grid>
-      <div className={classes.cardDetials}>
+      <div className={classes.cardDetails}>
         <Typography variant='h4'>
           Subtotal: {cart.subtotal.formatted_with_symbol}
         </Typography>
         <div>
-          <Button className={classes.emptyButton} size="large" type="button" variant='contained' color='secondary'>Empty Cart</Button>
+          <Button className={classes.emptyButton} size="large" type="button" variant='contained' color='secondary' onClick={handleEmptyCart}>Empty Cart</Button>
           <Button className={classes.checkoutButton} size="large" type="button" variant='contained' color='primary'>Checkout</Button>
         </div>
       </div>
